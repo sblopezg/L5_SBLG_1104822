9f97c62c646ef6b0246dfc304835d2d14e63de29
Estudiante <Estudiante@T203-15-2022>
2022-09-27 21:39:39 +0200

    Agregar archivos de proyecto.

---
+++ L5_SBLG_1104822/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace L5_SBLG_1104822
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero;
+
+            Console.WriteLine("EJERCICIO 1");
+            Console.WriteLine("Ingrese un Nùmero Entero" );
+            //"siguiente instruccion recibe y almacena numero"/
+
+            numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroEntero > 0)
+            {
+                Console.WriteLine("Resultado : positivo");
+            }
+            else if (numeroEntero < 0)
+            {
+                Console.WriteLine("Resultado : negativo");
+            }
+            else if (numeroEntero == 0)
+            {
+                Console.WriteLine("Resultado : es igual a cero");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
